788f4977f168ece139948c1bc70241ec3737d064
Gayle Ngozi <gaylengozi@gmail.com>
2023-02-10 14:19:52 +0000

    li element updated.

--- challenges/dog-photo-gallery/index.js
+++ challenges/dog-photo-gallery/index.js
@@ -1,24 +1,13 @@
-// fetch("https://dog.ceo/api/breeds/image/random")
-//   .then((response) => response.json())
-//   .then((data) => );
-
-//TRY: THIS IS A PROMISE. TELLING JS TO DO ONE THING, ELSE RETURN THE ERROR FOUND//
-
-// async function fetchDogImages (){
-//     try {
-//         const response = fetch("https://dog.ceo/api/breeds/image/random");
-//       const  fetchedData = await response.json();
-
-//GOBAL VARIABLES SHOULD BE PLACED OUTSIDE FUNTIONS//
-
 let button = document.querySelector("#image-button");
 let gallery = document.querySelector("#photo-album");
+let imagesArray = [];
 //set up ASYNC FUNCTION FOR FETCHING IMAGES FROM THE SPECIFIED API//
 
 async function fetchDogImages() {
   try {
     const response = await fetch(`https://dog.ceo/api/breeds/image/random`);
     const fetchedData = await response.json();
+    imagesArray.push(fetchedData);
     return fetchedData;
   } catch (error) {
     console.log(error);
@@ -31,14 +20,12 @@ button.addEventListener("click", displayDogImages);
 //ASYNC FUNCTION DISPLAYING THE IMAGES FETCHED/
 async function displayDogImages() {
   //creating an "img" element//
+const li = document.createElement("li");
   let images = document.createElement("img");
-
   imageFetched = await fetchDogImages();
   images.src = imageFetched.message;
-  const li = document.getElementById("image-li");
+  
   gallery.appendChild(li);
-  li.innerHTML = "";
-
   li.appendChild(images);
 }
 
